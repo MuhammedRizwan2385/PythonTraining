3671e3f0d3044a517f628c2a7a30ad44f57011e5
MuhammedRizwan2385 <mohammedrizwan2385@gmail.com>
2025-03-07 12:31:53 +0530

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,4 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+temp_degrees_centigrade=float(input())
+temp_fahrenheit=((9*temp_degrees_centigrade)/5)+32
+print(round(temp_fahrenheit,1))
