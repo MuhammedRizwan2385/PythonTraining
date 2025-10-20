0291694543277bb7c38bd7abe8b9ede083eb9575
M Muhammed Rizwan <mohammedrizwan2385@gmail.com>
2025-10-20 09:51:33 +0530

    Create leetcode_796_rotate_string.py

---
+++ leetcode_796_rotate_string.py
@@ -0,0 +1,27 @@
+'''Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+ 
+
+Example 1:
+
+Input: s = "abcde", goal = "cdeab"
+Output: true'''
+
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        if len(s)!=len(goal):
+            return False
+        else:
+            if goal in s+s:
+                return True
+            else:
+                return False
+       
